4750c1cf573f2bf169ae8e052ed0b4849b5cc6f8
tortuepin <kameyasu119@gmail.com>
2017-09-21 19:10:21 +0900

    test

--- rplugin/python3/denite/source/hierarchy.py
+++ rplugin/python3/denite/source/hierarchy.py
@@ -10,18 +10,45 @@ class Source(Base):
         super().__init__(vim)
         self.name = 'hierarchy'
         self.kind = 'hierarchy'
+        self.__wo = ""
 
-    # def on_init(self, context):
-        # TODO
+    def on_init(self, context):
+        context['__wo'] = 'hoho'
 
     # def on_close(self, context):
         # TODO
 
     def gather_candidates(self, context):
-        # TODO: Following code is a sample
-        candidates = ['1', '3', '5']
+        context['is_interactive'] = True
+        now_lnum = self.vim.call('line', '.')
+        args = ''
+        #if 'wo' in context['sources'][0].keys():
+        #    args = context['sources'][0]['args']
+        args = context['sources'][0]['args']
+        context['__wo'] = 'wooo'
+        if args:
+            arg = int(args[0])
+            denite_bufnr = self.vim.call('bufnr', '')
+            self.vim.command('buffer ' + str(context['bufnr']))
+            line_list = self.vim.call('workflowish#findSameRankLineList', 3)
+            self.vim.command('buffer ' + str(denite_bufnr))
+        else:
+            line_list = self.vim.call('workflowish#findSameRankLineList')
+            #line_list = self.vim.call('getwininfo')
+        
+        #win_id = self.vim.call('bufwinid', '%')
+        context['source'] = {"huga": "huga"}
+        line_list = context['source']
+        #line_list = context.keys()
+        #line_list = [context['prev_winid'], context['bufnr']]
+        #line_list.append("hgoe")
         return list(map(
-            lambda candidate: {
-                'word': candidate,
-                'action__num': "10"},
-            candidates))
+            lambda line: {
+                'word': str(line),
+                'action__hoge': "huhuhu"},
+            line_list))
+        #return list(map(
+        #    lambda line: {
+        #        'word': self.vim.call("workflowish#getline", int(line)),
+        #        'action__num': str(line)},
+        #    line_list))
